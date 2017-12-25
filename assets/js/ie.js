3777d41144ab2467e83f22ae91b141e7940ffeb8
mortalis13 <stangerroman@gmail.com>
2017-12-25 22:42:42 +0100

    - new

---
+++ assets/js/ie.js
@@ -0,0 +1,19 @@
+
+// $(function(){
+  
+//   // $(".menu-item").tooltip({ selector: "a[data-toggle=tooltip]" });
+
+//   // $(document).on('mouseenter', '[rel="tooltip"]', function() {
+//   //   alert('ok');
+    
+//   //     $(this).tooltip({
+//   //         container: 'body',
+//   //         trigger: 'manual'
+//   //     }).tooltip('show');
+//   // });
+  
+//   // $(document).on('mouseleave', '[rel="tooltip"]', function() {
+//   //     $(this).tooltip('hide');
+//   // });  
+  
+// })
