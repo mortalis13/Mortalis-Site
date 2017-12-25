ff0afa3becebe93a39d91537eb16e14d0ac024ba
mortalis13 <stangerroman@gmail.com>
2017-12-25 22:45:54 +0100

    v 1.0.1

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,32 +3,33 @@ $(function(){
   
   $("#contacts-menu a").click(function(){
     $(this).blur()
-  })
+  });
   
-  // $("#contacts-menu a").focus(function(){
-  //   $(this).tooltip('show')
-  // })
-  // $('[data-toggle="tooltip"]').tooltip({
-  //   trigger: 'manual'
-  // })
-  
-  $('[data-toggle="tooltip"]').tooltip()
+  $('[data-toggle="tooltip"]').tooltip();
   
   $('#projects .images a').hover(
     function(){
-      $(this).find('img').css({'opacity': 0.8})
+      $(this).find('img').css({'opacity': 0.8});
     },
     function(){
-      $(this).find('img').css({'opacity': 1})
+      $(this).find('img').css({'opacity': 1});
     }
-  )
+  );
   
   $('#nav').affix({
-      offset: {     
-        top: $('#nav').offset().top,
-        bottom: ($('footer').outerHeight(true) + $('.application').outerHeight(true)) + 40
-      }
+    offset: {
+      top: $('#nav').offset().top
+      // bottom: $('footer').outerHeight(true) + $('.application').outerHeight(true) + 40
+    }
   });
+  
+  $("#toTop").click(function(){
+    // $("html, body").animate({ scrollTop: 0 }, "slow");
+    $("html, body").scrollTop(0);
+    return false;
+  });
+  
+  // ------------------------------------------------------
   
   if (window.opera && window.opera.version() < 12) {
      document.documentElement.className += ' opera11';
