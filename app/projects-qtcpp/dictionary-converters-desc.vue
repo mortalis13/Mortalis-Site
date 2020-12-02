32814ca6a6ad1d86b9c0a3c7d328fe7ca24c3e5f
mortalis13 <stangerroman@gmail.com>
2020-12-02 23:35:46 +0100

    edit

--- app/projects-qtcpp/dictionary-converters-desc.vue
+++ app/projects-qtcpp/dictionary-converters-desc.vue
@@ -5,9 +5,9 @@
 &gt; stardict_to_dsl "c:\Star_Dict.ifo"</pre></p>
 <ul class="list-aligned project-modules-list">
 <li><a href="https://github.com/mortalis13/BGL-To-DSL-Converter" target="_blank"><b>BGL to DSL</b></a><br> 
-Parses binary BGL files and converts HTML code to DSL markup tags. <br>Based on the <a target="_blank" href="https://github.com/ilius/pyglossary"><b>pyglossary</b></a> and <a target="_blank" href="https://sourceforge.net/projects/ktranslator/files/dictconv/0.2"><b>dictconv</b></a> projects</li>
+Parses binary BGL files and converts HTML code to DSL markup tags. <br>Based on the <a target="_blank" href="https://github.com/ilius/pyglossary"><b>pyglossary</b></a> and <a target="_blank" href="https://sourceforge.net/projects/ktranslator/files/dictconv/0.2"><b>dictconv</b></a> projects.</li>
 <li><a href="https://github.com/mortalis13/StarDict-To-DSL-Converter" target="_blank"><b>StarDict to DSL</b></a><br>
-Extracts HTML code from the <b>.dict</b> compressed files and generates the DSL markup. <br>Based on the <a target="_blank" href="https://github.com/huzheng001/stardict-3"><b>stardict-3</b></a> project</li>
+Extracts HTML code from the <b>.dict</b> compressed files and generates the DSL markup. <br>Based on the <a target="_blank" href="https://github.com/huzheng001/stardict-3"><b>stardict-3</b></a> project.</li>
 </ul>
 </div>
 </template>
